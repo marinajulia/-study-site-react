0046f618410cc36945b7f6f4939c97cf9d4402ea
marinajulia <marinajulia707@gmail.com>
2022-03-19 15:26:10 -0300

    chore: Component props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
-function Social(){
-    return(
+const BemVindo = (props) => {
+    return (
+        <div>
+            <h3>Bem vindo {props.nome}</h3>
+            <h3>Tenho {props.idade} anos</h3>
+        </div>
+    );
+}
+function Social() {
+    return (
         <div>
             <h1>Ola mundo</h1>
+            <BemVindo nome="Matheus" idade="12" />
         </div>
     )
 }
