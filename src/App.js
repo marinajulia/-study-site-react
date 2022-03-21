6fe3aace6377ec5b26342f7469014e5579284f00
marinajulia <marinajulia707@gmail.com>
2022-03-21 20:55:53 -0300

    chore: Using components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
-import React from 'react';
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo}
-                idade={props.idade} />
-            <Social facebook={props.facebook} />
-            <hr/>
-        </div>
-    );
+import React, { Component } from "react";
+
+class Equipe extends Component {
+    render() {
+        return (
+          <Sobre name={this.props.name} cargo={this.props.cargo}/>
+        );
+    }
 }
 
-const Sobre = (props) => {
-    return (
-        <div>
-            <h2>Olá sou o {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade}</h3>
-        </div>
-    );
-}
-
-const Social = (props) => {
-    return (
-        <div>
-            <a href={props.facebook}>Facebook </a>
-            <a>Linkedin </a>
-            <a>Youtube</a>
-        </div>
-    );
+class Sobre extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Olá eu sou o {this.props.name}</h2>
+                <h2>Com o cargo {this.props.cargo}</h2>
+            </div>
+        );
+    }
 }
 
 function App() {
     return (
         <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Equipe nome="Matheus" cargo="programador" idade="30" facebook="www.teste.com" />
-            <Equipe nome="Carlos" cargo="Fisiculturista" idade="8" facebook="www.teste.com" />
+            <h1>Conheça nossa equipe</h1>
+            <Equipe name="Matheus" cargo="Desenvolvedor" />
         </div>
     );
 }
---
+++ src/AppAntigo.js
@@ -0,0 +1,43 @@
+// import React from 'react';
+// const Equipe = (props) => {
+//     return (
+//         <div>
+//             <Sobre nome={props.nome} cargo={props.cargo}
+//                 idade={props.idade} />
+//             <Social facebook={props.facebook} />
+//             <hr/>
+//         </div>
+//     );
+// }
+
+// const Sobre = (props) => {
+//     return (
+//         <div>
+//             <h2>Olá sou o {props.nome}</h2>
+//             <h3>Cargo: {props.cargo}</h3>
+//             <h3>Idade: {props.idade}</h3>
+//         </div>
+//     );
+// }
+
+// const Social = (props) => {
+//     return (
+//         <div>
+//             <a href={props.facebook}>Facebook </a>
+//             <a>Linkedin </a>
+//             <a>Youtube</a>
+//         </div>
+//     );
+// }
+
+// function App() {
+//     return (
+//         <div>
+//             <h1>Conheça nossa equipe:</h1>
+//             <Equipe nome="Matheus" cargo="programador" idade="30" facebook="www.teste.com" />
+//             <Equipe nome="Carlos" cargo="Fisiculturista" idade="8" facebook="www.teste.com" />
+//         </div>
+//     );
+// }
+
+// export default App;
