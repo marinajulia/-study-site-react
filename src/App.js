d327fab1fb8870b2b9427088439c1c846741d4c0
marinajulia <marinajulia707@gmail.com>
2022-03-27 17:49:08 -0300

    chore: folder Components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import Membro from './Components/Membro';
 
-class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            hora: '00:00:00'
-        };
-    }
-    componentDidMount(){
-        setInterval(()=>{
-            this.setState({hora: new Date().toLocaleTimeString()})
-        }, 1000)
-    }
-
-    componentDidUpdate(){
-        console.log("atualizou")
-    }
-
-    render() {
-        return (
+class App extends Component{
+    render(){
+        return(
             <div>
-                <h1>Meu projeto {this.state.hora}</h1>
+                <Membro nome="Visitante"/>
             </div>
-        );
+        )
     }
 }
 
