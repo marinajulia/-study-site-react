0805361cc9c245db50f5c05d46e48067bafad95b
marinajulia <marinajulia707@gmail.com>
2022-03-28 22:37:46 -0300

    chore: Finalize project

--- Projeto biscoito da sorte/biscoito/src/App.js
+++ Projeto biscoito da sorte/biscoito/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './estilo.css';
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      textoFrase: ''
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = ['frase1', 'frase2', 'frase3']
+  }
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = this.frases[numeroAleatorio];
+    this.setState(state);
+  }
+  render(){
+    return(
+      <div className="container">
+        <img className="img" src={require('./Assets/biscoito.png')} alt="biscoito"/>
+        <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
 }
-
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button id="botao" onClick={this.props.acaoBtn}> {this.props.nome}</button>
+      </div>
+    )
+  }
+}
 export default App;
