31dccdc978fcd5c21f0b246427fb8687583f1c1b
marinajulia <marinajulia707@gmail.com>
2022-03-30 21:43:33 -0300

    chore: Add style

--- Cronometro/cronometro/src/App.js
+++ Cronometro/cronometro/src/App.js
@@ -1,16 +1,22 @@
-import React, {Component} from "react";
-class App extends Component{
+import React, { Component } from "react";
+import './style.css'
+class App extends Component {
   constructor(props){
     super(props)
     this.state ={
+      numero: 0
     }
-
   }
- 
-  render(){
-    return(
-      <div>
-        
+
+  render() {
+    return (
+      <div className="container">
+        <img src={require('./Assets/cronometro.png')} className="img" alt="" />
+        <a className="timer">{this.state.numero.toFixed()}</a>
+        <div className="areaBtn">
+          <a className="botao">VAI</a>
+          <a className="botao">LIMPAR</a>
+        </div>
       </div>
     );
   }
