f160dcdc99fd415e289d4fe777a514ccd13a16ab
marinajulia <marinajulia707@gmail.com>
2022-03-30 21:55:51 -0300

    chore: Add go

--- Cronometro/cronometro/src/App.js
+++ Cronometro/cronometro/src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
 import './style.css'
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state ={
+    this.state = {
       numero: 0
+    };
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+  vai() {
+
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state)
+      }, 100)
     }
   }
+  limpar() {
 
+  }
   render() {
     return (
       <div className="container">
         <img src={require('./Assets/cronometro.png')} className="img" alt="" />
-        <a className="timer">{this.state.numero.toFixed()}</a>
+        <a className="timer">{this.state.numero.toFixed(1)}</a>
         <div className="areaBtn">
-          <a className="botao">VAI</a>
-          <a className="botao">LIMPAR</a>
+          <a className="botao" onClick={this.vai}>VAI</a>
+          <a className="botao" onClick={this.limpar}>LIMPAR</a>
         </div>
       </div>
     );
