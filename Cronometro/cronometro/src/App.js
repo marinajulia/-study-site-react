e216551821c795ce558d152516960dd3fd3c5108
marinajulia <marinajulia707@gmail.com>
2022-04-02 14:54:23 -0300

    chore: add cronometro

--- Cronometro/cronometro/src/App.js
+++ Cronometro/cronometro/src/App.js
@@ -4,34 +4,50 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      numero: 0
+      numero: 0,
+      botao: 'GO'
     };
+    this.timer = null;
     this.vai = this.vai.bind(this);
     this.limpar = this.limpar.bind(this);
   }
+
   vai() {
+    let state = this.state;
 
     if (this.timer !== null) {
       clearInterval(this.timer);
       this.timer = null;
+      state.botao = 'GO'
     } else {
       this.timer = setInterval(() => {
         let state = this.state;
         state.numero += 0.1;
         this.setState(state)
-      }, 100)
+      }, 100);
+      state.botao = 'PAUSE'
     }
+    this.setState(state)
   }
-  limpar() {
 
+  limpar() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    let state = this.state;
+    state.numero = 0;
+    state.botao = 'GO';
+    this.setState(state);
   }
+
   render() {
     return (
       <div className="container">
         <img src={require('./Assets/cronometro.png')} className="img" alt="" />
         <a className="timer">{this.state.numero.toFixed(1)}</a>
         <div className="areaBtn">
-          <a className="botao" onClick={this.vai}>VAI</a>
+          <a className="botao" onClick={this.vai}>{this.state.botao}</a>
           <a className="botao" onClick={this.limpar}>LIMPAR</a>
         </div>
       </div>
